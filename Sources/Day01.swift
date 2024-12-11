0f9dc9facd8d15aea85db251156d43e3909d9ba1
Nikolai Ruhe <git@nikolai-ruhe.de>
2024-12-11 11:26:03 +0100

    Day01

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -3,16 +3,65 @@ import Algorithms
 // https://adventofcode.com/2023/day/1
 struct Day01: AdventDay {
   static let testData = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
     """
 
-  static var expectedTestResult1:   String { "unknown" }
-  static var expectedProperResult1: String { "unknown" }
-  static var expectedTestResult2:   String { "unknown" }
-  static var expectedProperResult2: String { "unknown" }
+  static let testData2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+  static var expectedTestResult1:   String { "142" }
+  static var expectedProperResult1: String { "54561" }
+  static var expectedTestResult2:   String { "281" }
+  static var expectedProperResult2: String { "54076" }
 
   var data: String
-  init(data: String) { self.data = data }
 
-  func part1() -> Any { "not implemented" }
-  func part2() -> Any { "not implemented" }
+  func part1() -> Any {
+    var result = 0
+
+    var first: Int? = nil
+    var last: Int? = nil
+
+    for character in data {
+      if character.isNumber {
+        guard let value = Int(String(character)) else { continue }
+        if first == nil {
+          first = value
+        } else {
+          last = value
+        }
+      } else if character.isNewline {
+        result += 10 * (first ?? 0) + (last ?? first ?? 0)
+        first = nil
+        last = nil
+      }
+    }
+
+    result += 10 * (first ?? 0) + (last ?? first ?? 0)
+    return result
+  }
+
+  func part2() -> Any {
+    let numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    func convert(_ input: Substring) -> Int {
+      let string = String(input)
+      return Int(string) ?? numbers.firstIndex(of: string)! + 1
+    }
+
+    let regex = /one|two|three|four|five|six|seven|eight|nine|\d/
+    return data.split(separator: "\n").map { line in
+      let matches = line.matches(of: regex)
+      return matches.isEmpty ? 0 : 10 * convert(matches.first!.0) + convert(matches.last!.0)
+    }.reduce(0, +)
+  }
 }
